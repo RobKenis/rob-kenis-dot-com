f6be7b61bf7fc13f6bf04390903e4bbed1491eb0
Rob <rob.kenis@hotmail.com>
2019-02-03 13:29:43 +0100

    Add font-awesome and bootstrap dependencies
    And fix stupid lint errors

--- app/src/main.js
+++ app/src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
 
-Vue.config.productionTip = false
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  render: h => h(App)
+});
