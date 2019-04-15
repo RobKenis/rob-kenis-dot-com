c10d47ca48bf8695dd8f779e17813602a22e50f8
Rob Kenis <rob.kenis@vrt.be>
2019-04-15 19:06:02 +0200

    And fix the damn test

--- jest.config.js
+++ jest.config.js
@@ -4,10 +4,11 @@ module.exports = {
     '^~/(.*)$': '<rootDir>/$1',
     '^vue$': 'vue/dist/vue.common.js'
   },
-  moduleFileExtensions: ['js', 'vue', 'json'],
+  moduleFileExtensions: ['js', 'ts', 'vue', 'json'],
   transform: {
-    '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest'
+    "^.+\\.js$": "<rootDir>/node_modules/babel-jest",
+    '.*\\.(vue)$': 'vue-jest',
+    "^.+\\.tsx?$": "ts-jest"
   },
   collectCoverage: true,
   collectCoverageFrom: [
