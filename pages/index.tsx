4229a7913e0e05eab3ecc11309ae5e95db2c2343
Rob Kenis <rob.kenis@vrt.be>
2020-02-05 22:07:21 +0100

    Add header with links to blog and social media

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,37 @@
 import Head from 'next/head';
+import { NavBar } from '../components/navbar';
+import FontFaceObserver from 'fontfaceobserver';
+import React from 'react';
 
-const App = () => (<div>
-  <Head>
-    <title>Rob Kenis</title>
-    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    <link rel="Shortcut Icon" type="image/x-icon" href="favicon.ico" />
-  </Head>
-  <div> Welcome to Next.js! </div>
-</div>);
+const Fonts = () => {
+  const link = document.createElement('link')
+  link.href = 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900'
+  link.rel = 'stylesheet'
+
+  document.head.appendChild(link)
+
+  const roboto = new FontFaceObserver('Roboto')
+
+  roboto.load().then(() => {
+    document.documentElement.classList.add('roboto')
+  })
+}
+
+class App extends React.Component {
+  componentDidMount() {
+    Fonts()
+  }
+
+  render() {
+    return <div>
+      <Head>
+        <title>Rob Kenis</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="Shortcut Icon" type="image/x-icon" href="favicon.ico" />
+      </Head>
+      <NavBar />
+    </div>
+  }
+}
 
 export default App;
