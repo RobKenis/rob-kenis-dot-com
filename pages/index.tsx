3dd9d4b6a07de6049e692e9aac82cfa5d422e86f
Rob Kenis <rob.kenis@vrt.be>
2020-02-05 18:28:33 +0100

    Init nextjs project

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+const App = () => (<div>
+  <Head>
+    <title>Rob Kenis</title>
+    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    <link rel="Shortcut Icon" type="image/x-icon" href="favicon.ico" />
+  </Head>
+  <div> Welcome to Next.js! </div>
+</div>);
+
+export default App;
