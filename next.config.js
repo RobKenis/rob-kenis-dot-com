c3ad1cc7d22dc737eecbc94dbc9a2ab04c1f9ae9
Rob Kenis <rob.kenis@vrt.be>
2020-02-07 21:00:19 +0100

    Add antd and move fonts to less

---
+++ next.config.js
@@ -0,0 +1,16 @@
+const withCSS = require('@zeit/next-css')
+const withSass = require('@zeit/next-sass')
+const withLess = require('@zeit/next-less')
+
+const isProd = process.env.NODE_ENV === 'production'
+
+// fix: prevents error when .less files are required by node
+if (typeof require !== 'undefined') {
+  require.extensions['.less'] = file => { }
+}
+
+module.exports = withLess(withSass(withCSS({
+  lessLoaderOptions: {
+    javascriptEnabled: true
+  }
+})))
