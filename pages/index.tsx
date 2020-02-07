482a5376aec2f02c75287aa4a94df87ab1c022e1
Rob Kenis <rob.kenis@vrt.be>
2020-02-07 21:50:25 +0100

    Make the empty site look pretty

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import Head from 'next/head';
-import { NavBar } from '../components/navbar';
 import React from 'react';
-import FeaturedContent from '../components/featuredContent';
+import { Layout, Empty } from 'antd';
+const { Header, Footer, Content } = Layout;
 
 import '../css/antd.less'
+import { NavBar } from '../components/navbar';
 
 const App = () => (
   <div>
@@ -12,8 +13,11 @@ const App = () => (
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <link rel="Shortcut Icon" type="image/x-icon" href="favicon.ico" />
     </Head>
-    <NavBar />
-    <FeaturedContent />
+    <Layout>
+      <Header><NavBar/></Header>
+      <Content style={{padding: '2em'}}><Empty/></Content>
+      <Footer style={{display: 'flex', justifyContent: 'center'}}>Rob Kenis ©2020</Footer>
+    </Layout>
   </div>
 )
 
