5e1bfaec9c4a205ef0e32bb7cf480a3354296756
Rob Kenis <rob.kenis@hotmail.com>
2020-10-24 19:10:54 +0200

    Add permissions for github to manage S3 and Cloudfront

--- deploy/aws/templates/github.py
+++ deploy/aws/templates/github.py
@@ -30,6 +30,26 @@ template.add_resource(User(
                     Resource=['*'],
                 )]
             )
+        ),
+        Policy(
+            PolicyName='AllowS3',
+            PolicyDocument=PolicyDocument(
+                Statement=[Statement(
+                    Effect=Allow,
+                    Action=[Action(prefix='s3', action='*')],
+                    Resource=['*'],
+                )]
+            )
+        ),
+        Policy(
+            PolicyName='AllowCloudFront',
+            PolicyDocument=PolicyDocument(
+                Statement=[Statement(
+                    Effect=Allow,
+                    Action=[Action(prefix='cloudfront', action='*')],
+                    Resource=['*'],
+                )]
+            )
         )
     ]
 ))
