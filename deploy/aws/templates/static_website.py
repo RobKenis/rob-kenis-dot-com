1578ae8cfcaa2b9c80232a6b0cb920db36866acf
Rob Kenis <rob.kenis@hotmail.com>
2020-10-24 11:43:27 +0200

    Add ACM Certificate for CloudFront

--- deploy/aws/templates/static_website.py
+++ deploy/aws/templates/static_website.py
@@ -1,6 +1,31 @@
-from troposphere import Template
+from troposphere import Template, ImportValue, Parameter, constants, Join, Ref
+from troposphere.certificatemanager import Certificate, DomainValidationOption
 
 template = Template(Description="Static Website")
+
+dns_stack = template.add_parameter(Parameter(
+    'DnsStack',
+    Type=constants.STRING,
+    Default='rob-kenis-dot-com-dns',
+    Description='Name of the CloudFormation stack that holds the HostedZone',
+))
+
+template.add_resource(Certificate(
+    "CloudFrontCertificate",
+    DomainName='robkenis.com',
+    SubjectAlternativeNames=['www.robkenis.com'],
+    ValidationMethod='DNS',
+    DomainValidationOptions=[
+        DomainValidationOption(
+            DomainName='robkenis.com',
+            HostedZoneId=ImportValue(Join('-', [Ref(dns_stack), 'HostedZoneId'])),
+        ),
+        DomainValidationOption(
+            DomainName='www.robkenis.com',
+            HostedZoneId=ImportValue(Join('-', [Ref(dns_stack), 'HostedZoneId'])),
+        ),
+    ],
+))
 
 f = open("output/static_website.json", "w")
 f.write(template.to_json())
