b30846feae5785583ef68f627cab8d480575c99f
Rob Kenis <rob.kenis@nike.com>
2021-11-03 20:43:20 +0100

    Make post title a little bolder

--- components/layouts/Post.tsx
+++ components/layouts/Post.tsx
@@ -3,6 +3,9 @@ import { Title } from "../elements/Title/Title";
 import { Row } from "antd";
 import styles from './Post.module.css';
 import { PostedBy } from "../elements/PostedBy/PostedBy";
+import { CSSProperties } from "react";
+
+const titleCSS: CSSProperties = { fontFamily: 'Lato, serif', margin: '1em 0 2em 0', fontWeight: "bold" }
 
 const Post = ({ meta, children }) => {
   return (
@@ -12,7 +15,7 @@ const Post = ({ meta, children }) => {
           <PostedBy author={meta.author} created={meta.created}/>
         </Row>
         <Row>
-          <Title title={meta.title} style={{fontFamily: 'Lato, serif', margin: '1em 0 2em 0'}}/>
+          <Title title={meta.title} style={titleCSS}/>
         </Row>
         <Row>
           <main className={styles.content}>{children}</main>
